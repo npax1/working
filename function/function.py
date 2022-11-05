b63f87d5b2c01a2604f719b815cff35024800174
npax1 <prakh55555@gmail.com>
2022-11-05 12:54:34 +0200

    add code

--- function/function.py
+++ function/function.py
@@ -1,2 +1,7 @@
-def
-nums=[int(i) for i in arr]
+def ex_3 (arr):
+    nums=[int(i) for i in arr]
+    nums_sum=0
+    for i in nums:
+        nums_sum+=i
+    mean=nums_sum / len(nums)
+    return f'Sum = {nums_sum}, arti mean= {mean}'
