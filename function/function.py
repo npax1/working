cdf0a1d77b90a101a47f0df4ca701b52f2e07eb6
npax1 <prakh55555@gmail.com>
2022-11-05 12:07:17 +0200

    add code

--- function/function.py
+++ function/function.py
@@ -1,13 +1,1 @@
-sentence = '''
-Meet my family. There are five of us – my parents, my elder brother, my baby sister and me. First, meet my mum and dad, Ja
-ne and Michael. My mum enjoys reading and my dad enjoys playing chess with my brother Ken. My mum slim and is rather tall. She has long red hair and 
-'''
-
-words=sentence.split()
-print(words)
-for word in words:
-    if sentence.count(word)>0:
-        origen=word
-        temp=word.upper()
-        sentence=sentence.replace(origen, temp)
-print(sentence)
+taxti=input('text&&&&')
