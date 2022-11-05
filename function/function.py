d75339bd7184e334ad2a6da17dc28450c0ee812d
npax1 <prakh55555@gmail.com>
2022-11-05 12:19:36 +0200

    add code

--- function/function.py
+++ function/function.py
@@ -1,12 +1,2 @@
-s=int(input("add text"))
-a=int(input("add text"))
-d=int(input("add text"))
-num_list = [s, a, d]
-res = sum(num_list)
-avg = res / len(num_list)
-print("sum is: ", res, "Average is: ", avg)
-res1 = 0
-for num in num_list:
-    res1 += num
-avg1 = res1 / len(num_list)
-print("sum is: ", res1, "Average is: ", avg1)
+def
+nums=[int(i) for i in arr]
